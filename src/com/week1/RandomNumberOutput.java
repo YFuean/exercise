50b6fedb993dcfc5ded6f036a6ae23903014fc36
yuefan <1412666584@qq.com>
2020-01-13 22:02:47 +0800

    exercise1

--- src/com/week1/RandomNumberOutput.java
+++ src/com/week1/RandomNumberOutput.java
@@ -1,34 +1,37 @@
 package com.week1;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * @ClassName RandomNumberOutput
- * @Description 随机生成100个0-99之间的随机整数存入数组，选取字符输出流写入本地文件
+ * @Description 随机生成100个0-99之间的随机整数存入数组，选取字节输出流写入本地文件
  * @Author yue_fan
  * @Date 2020/1/13
  **/
 public class RandomNumberOutput {
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=1;i<=100;i++){
-            int num = new Random().nextInt(100);
-            list.add(num);
+    public static void main(String[] args) throws IOException {
+        //List<Integer> list = new ArrayList<>();
+        StringBuffer values = new StringBuffer();
+        for (int i = 1; i <= 100; i++) {
+            int number = (int) (Math.random() * 100);
+            //list.add(number);
+            values.append(" "+number);
         }
-        try {
-            File file = new File("D:\\number.txt");
-            FileOutputStream os = new FileOutputStream(file);
-            for (Integer integer:list){
-                os.write(integer);
+        File file = new File("D:/number.txt");
+        if (!file.exists()) {
+            try {
+                FileWriter fw = new FileWriter(file);
+                fw.write(String.valueOf(values));
+                fw.close();
+                System.out.println("写入成功！");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            os.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        else {
+            System.out.println("文件已存在！");
         }
     }
 }
